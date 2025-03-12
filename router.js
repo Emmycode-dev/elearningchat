90cecf66911c5048f583369c13481cd72abac49a
Emmycode-dev <sylvestere081@gmail.com>
2025-03-12 17:31:48 +0100

    Update router.js

--- router.js
+++ router.js
@@ -2,14 +2,14 @@ var express = require("express");
 var router =express.Router();
 
 const credential={
-    email:"admin@gmail.com",
-    password:"admin123"
+    matricnumber:"230100111004",
+    username:"kingsley",
 }
 
 //login user
 router.post('/login',(req,res)=>{
-    if(req.body.email==credential.email&&req.body.password==credential.password){
-        req.session.user=req.body.email;
+    if(req.body.matricnumber==credential.matricnumber&&req.body.username==credential.username){
+        req.session.user=req.body.username;
         res.redirect('/route/dashboard');
         //res.end("Login Succesful...!");
     }else{
